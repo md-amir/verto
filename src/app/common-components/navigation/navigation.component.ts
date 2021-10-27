3cc449e4473b9358f1e3488840480d0f0d17aa34
amir <amir.ict.engg@gmail.com>
2021-10-28 01:17:42 +0600

    nav going on

--- src/app/common-components/navigation/navigation.component.ts
+++ src/app/common-components/navigation/navigation.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent implements OnInit {
+  header: HTMLElement | undefined;
+  verto: HTMLElement | undefined;
+  constructor(@Inject(DOCUMENT) private document: Document) {
 
-  constructor() { }
-
-  ngOnInit(): void {
   }
 
+  ngOnInit(): void {
+
+  }
+
+  handlelHeaderToggle(event: Event): void {
+    (event.currentTarget as HTMLElement).classList.toggle('bx-x');
+    this.header = document.getElementById('header') as HTMLElement;
+    this.verto = document.getElementById('verto-body') as HTMLElement;
+
+    this.verto.classList.toggle('verto-body');
+    this.verto.classList.toggle('verto-body-narrow');
+  }
 }
--- src/app/common-components/side-bar/side-bar.component.ts
+++ src/app/common-components/side-bar/side-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-side-bar',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) private document: Document) { }
 
   ngOnInit(): void {
+  }
+
+  handleNavLinkClick(event: Event) {
+    var linkColor = this.document.querySelectorAll('.nav_link') as NodeList;
+    if (linkColor) {
+      linkColor.forEach(l => {
+        var a = l as HTMLElement
+        a.classList.remove('nav-active')
+      })
+      // this.classList.add('active')
+    }
+    (event.currentTarget as HTMLElement).classList.toggle('nav-active');
   }
 
 }
